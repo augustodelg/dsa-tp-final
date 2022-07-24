81380ae63ce195608222b83648f5a69be83d7e1f
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-07-23 21:41:33 -0300

    Config cors

--- back-end/config/initializers/cors.rb
+++ back-end/config/initializers/cors.rb
@@ -10,8 +10,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins ENV['FRONT_APP_URL']
 
     resource '*',
-      :headers => :any,
+      headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      credentials: true 
   end
 end
